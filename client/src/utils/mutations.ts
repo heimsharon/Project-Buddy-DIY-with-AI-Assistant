8213518455fd8c9b2286d13fd3eb4e79ca001bc1
heimsharon <heimsharon@hotmail.com>
2025-12-14 21:33:20 -0500

    added project mutations

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -9,21 +9,19 @@ export const LOGIN_USER = gql`
         username        
       }
     }
-  }
-`;
+  }`;
 
-        export const CREATE_USER = gql`
-        mutation crateUser($username: String!, $email: String!, $password: String!) {
-          createUser (username: $username, email:$email, password: $password) {
-            user {
-              username
-              email
-              _id
-            }
-              token 
-              }
-              }
-`; 
+export const CREATE_USER = gql`
+  mutation crateUser($username: String!, $email: String!, $password: String!) {
+    createUser (username: $username, email:$email, password: $password) {
+      user {
+        username
+        email
+        _id
+      }
+    token 
+    }
+  }`; 
 
 export const UPDATE_USER = gql`
   mutation updateUser(
@@ -43,8 +41,7 @@ export const UPDATE_USER = gql`
         email
       } 
     )
-    }
-`;
+  }`;
 
 export const DELETE_USER = gql`
   mutation deleteUser {
@@ -53,6 +50,95 @@ export const DELETE_USER = gql`
       username
       email
     }
-  }
-`;
+  }`;
 
+export const CREATE_PROJECT = gql`
+  mutation createProject(
+    $title: String!,
+    $description: String,
+    $type: String,
+    $estimatedBudget: Float,
+    $dimensions: DimensionsInput,
+    $completionGoalDate: String,
+    $materialIds: [ID],
+    $userID: ID!
+  ) {
+    createProject(
+      title: $title,
+      description: $description,
+      type: $type,
+      estimatedBudget: $estimatedBudget,
+      dimensions: $dimensions,
+      completionGoalDate: $completionGoalDate,
+      materialIds: $materialIds,
+      userId: $userId
+    ) {
+      _id
+      title
+      description
+      type
+      estimatedBudget
+      dimensions {
+        length
+        width
+        height
+        depth
+      }
+      completionGoalDate
+    }  
+  }`;
+
+export const UPDATE_PROJECT = gql`
+mutation updateProject(
+  $id: ID!,
+  $title: String,
+  $description: String,
+  $type: String,
+  $estimatedBudget: Float,
+  $dimensions: DimensionsInput,
+  $completionGoalDate: String,
+  $materialIds: [ID!]
+) {
+  updateProject(
+    id: $id,
+    title: $title,
+    description: $description,
+    type: $type,
+    estimatedBudget: $estimatedBudget,
+    dimensions: $dimensions,
+    materialIds: $materialIds
+  ) {
+    _id
+    title
+    description
+    type
+    estimatedBudget,
+    dimensions {
+      length
+      width
+      height
+      depth
+    }
+    completionGoalDate
+  }
+}`;
+
+export const DELETE_PROJECT = gql`
+mutation deleteProject($id: ID!) {
+  deleteProject(id: $id) {
+    _id
+    title
+    description
+    estimatedBudget
+    type
+    dimensions {
+      length
+      width
+      height
+      depth
+    }
+    completionGoalDate
+  }
+}`
+
+)`
