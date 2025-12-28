027065cedde668187292041581955496e4af2e75
heimsharon <heimsharon@hotmail.com>
2025-12-28 18:41:58 -0500

    working on homepage and loginpage

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,7 +2,37 @@ import { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 
+export default function homepage() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-export default homepage() {
+  useEffect(() => {
+    getProfile().then(profile => setUser(profile))
+      .catch(() => navigate('/login'));
+  } [navigate]);
 
+  if (!user) {
+    return <div> Loading....</div>;
+  }
+
+  return (
+    <div className="dashboard__background">
+      <div className="homepage__hero-content">
+        <h1> Welcome Home</h1>
+        <p> Add Text.... Also Icons/emojis.</p>
+        <Link to="/projectcreate"
+          className="homepage__btn--call-to-action"
+        > Create A Project
+        </Link>
+      </div>
+
+      <h2
+      > Features
+      </h2>
+      <div className="dashboard__features">
+
+      </div>
+    </div>
+  );
 }
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,28 +1,88 @@
 
-import { useState, type FormEvent, type ChangeEvent} from "react";
+import { useState, type FormEvent, type ChangeEvent } from "react";
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 
-const Login = () => {
-  const[formState, setFormState] = useState({email:'', password:''});
-  const[login, {error, data, loading}] = useLoginMutation(LOGIN_USER);
 
+export default function LoginPage() {
+  const navigate = useNavigate();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormState((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleFormSubmit = async (e: FormEvent) => {
+  //Bypass full login
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    try {
-      const { data } = await login(
-        { variables: {...formState},
-        }
-      );
-      
-      
-      
+    navigate("/dashboard");
+  }
 
 
-    }}}
+  //Full login commented out to bypass login during development
+  //const Login () => {
+  //const [formState, setFormState] = useState({ email: '', password: '' });
+  //const [login, { error, data, loading }] = useLoginMutation(LOGIN_USER);
+
+  //const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //const { name, value } = e.target;
+  //setFormState((prev) => ({ ...prev, [name]: value }));
+  //};
+
+  //const handleFormSubmit = async (e: FormEvent) => {
+  //e.preventDefault();
+  //try {
+  //const { data } = await login(
+  //{
+  //variables: { ...formState },
+  //}
+  //);
+  //Auth.login(data.login.token);
+  //} catch (e) {
+  //}
+  //setFormState({
+  //email:'',
+  //password:'',
+  //})
+  //};
+
+  return (
+    <main className="login__background">
+      <div className="login"
+        aria-label="Login Page with links to registration and forgot password">
+        <div "login-logo"> </div>
+      <header className="login__card-header"
+      > Project Buddy
+      </header>
+
+      <form className="form-input">
+        {data ? (
+          <p>
+            Success! You May Now Head{''}
+            <link to="/"
+            > To Your Dashboard.</Link>
+          </p>
+        ) : (
+
+          <form onSubmit={handleSubmit}
+            autoComplete="off">
+
+            <fieldset className="login__fieldset">
+            </fieldset>
+            <legend className="sr-only"
+            > Login Form
+            </legend>
+//////////////</form>
+
+
+            <label htmlFor="email"
+            
+            > Email
+            </label>
+            <Input
+              className="login__card-input"
+
+    </main>
+        )
+
+
+
+
+
+        }}
+}
