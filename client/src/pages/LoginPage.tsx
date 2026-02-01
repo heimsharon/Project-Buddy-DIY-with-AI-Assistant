209a7cd1947626537edced1c19fa4081783bf888
heimsharon <heimsharon@hotmail.com>
2026-02-01 14:48:33 -0500

    login changes

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 
-import { useState, use Navigate, type FormEvent, type ChangeEvent } from "react";
+import { useState, useNavigate, type FormEvent, type ChangeEvent } from "react";
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
 
@@ -45,65 +45,70 @@ export default function LoginPage() {
     <div className="login__background">
       <main className="login"
         aria-label="Login Page with links to registration and forgot password">
-        <div "login-logo"> </div>
-      <header className="login__card-header"
-      > Project Buddy
-      </header>
+        <div className="login-logo"> </div>
+        <header className="login__card-header"
+        > Project Buddy
+        </header>
 
-      <form className="form-input"
-            onSubmit={handleSubmit}
-            autoComplete="off">
-        {data ? (
-          <p>
-            Success! You May Now Head{''}
-            <Link to="/"
-            > To Your Dashboard.</Link>
-          </p>
-        ) : (         
+        <form className="form-input"
+          onSubmit={handleSubmit}
+          autoComplete="off">
+          {data ? (
+            <p>
+              Success! You May Now Head{''}
+              <Link to="/"
+              > To Your Dashboard.
+              </Link>
+            </p>
+          ) : (         
 
             <fieldset className="login__fieldset">
-                <legend className="sr-only"
-            > Login Form
-            </legend>
-            </fieldset>
-          
+              <legend className="sr-only"
+              > Login Form
+              </legend>
+            </fieldset>          
 
             <label htmlFor="email"
             > Email
             </label>
-            <input... />
+            <input>
+            </input>           
 
             <label htmlFor="password"
             > Password
             </label>
-            <input.... />
-            <div className="remember-me--checkbox">
-              <input... />
-              <label...>Remember Me </label>
+            
+            <div className="remember-me--checkbox">             
+              <label htmlFor= "remember-me"
+              > Remember Me 
+              </label>
             </div>
+
             <button type="submit"
             className="btn--primary"
             > Login
             </button>
-            </fieldset >
+            
             </form >
-    <div className="link__forgot-password">
-      <Link to="/forgot__password"
-        className="btn"
-      > Forgot Password?
-      </Link>
-      <Link to "registration"
-      className="btn"
+
+
+        <div className="link__forgot-password">
+          <Link to="/forgot__password"
+            className="btn"
+          > Forgot Password?
+          </Link>
+
+          <Link to "registration"
+          className="btn"
               > Need to Create an Account?
-    </Link>
-            </div >     
-          </main >
-          </div >
+          </Link>
+        </div>
+        
+      
+      </main >
+    </div >
         )
-
-
-}
-      };
-
+  ;
+      }
 
 
