4946c63323e1b3ac0f17f5e243a27543399a4dda
heimsharon <heimsharon@hotmail.com>
2026-02-08 11:00:10 -0500

    loginpage clean up--cleaner login bypass for development

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,2 +1,15 @@
 import ( useEffect, useState ) from 'react';
-import 
+
+import (type UserData) from '../';
+
+export default function ProfilePage() {
+  const [ userData, setUserData] = useState<UserData | null>(null);
+  const [ editMode, setEditMode] = useState(false);
+  const [ isSuccess, setIsSuccess ] = useState(false);
+  const [ error, setError ] = useState<string | null>(null);
+  const [ isLoading ] = useState(false);
+
+
+
+
+
