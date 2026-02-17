b3b6a02873e9700f5f85adc50824f9034831dd77
heimsharon <heimsharon@hotmail.com>
2026-02-16 20:15:29 -0500

    project type, progress enum, dimensions type

--- client/src/pages/CreateProjectPage.tsx
+++ client/src/pages/CreateProjectPage.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
+import { ProjectData } from "@/types/project";
 
 
 export default function CreateProject(){
 
-
-const [ isLoading, setIsLoading]
+const [form, setForm] = useState<ProjectData | null>(null);
+const [isLoading, setIsLoading] = useState (false);
 const [isSuccess, setIsSuccess] = useState(false);
 const [error, setError] = useState<string | null>(null);
 }
--- client/src/types/project.ts
+++ client/src/types/project.ts
@@ -1,8 +1,31 @@
-export interface ProjectData {
-  id: number 
-  name: string
-  userId: number
-  userName: string 
-  
+export enum ProjectStatus {
+  Planning = 'planning',
+  Active = 'active',
+  WrappingUp = 'wrapping_up',
+  OnHold = 'on_hold',
+  Completed = 'completed',
+  Canceled = 'cancelled',
+}
 
+export type Dimensions = {
+  length: number;
+  width: number;
+  height?: number;
+  unit: "cm" | "in" | "m" | "ft";
+}
+
+export interface ProjectData {
+  id: number;
+  userId: number;
+  userName: string;
+  projectName: string;
+  description?: string;
+  dimensions?:Dimensions;
+  createdAt: Date;
+  updatedAt?: Date;
+  startDay?: Date;
+  projectEndDate?: Date;
+  projectBudget?: number;
+  materials?: Material[];
+  status: ProjectStatus;
 }
